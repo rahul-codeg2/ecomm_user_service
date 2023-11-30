b27ccbccef9cf0dbbbb1059679720083f469c4a0
rahul-codeg2 <rahul.hardwin@gmail.com>
2023-11-30 17:28:18 +0530

    cors fixes

--- src/main/java/com/ecomm_app/config/CorsConfig.java
+++ src/main/java/com/ecomm_app/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
                 .allowCredentials(true)
